ac949e2c34f1aab7915f74c9967e300f77c25d1b
JonusBurger <j.bergner@live.de>
2025-02-17 19:46:17 +0100

    start handle of Eventlistener for addNote Form

--- src/buildPage/buildForm.js
+++ src/buildPage/buildForm.js
@@ -1,7 +1,9 @@
 export default function buildFormElement() {
     const formElement = document.getElementById("formContainer");
 
+    const addNoteFormElement = document.getElementById("noteBox");
     const addNoteBox = document.querySelector(".addNoteBox");
+    const addNoteButton = document.getElementById("addNoteBtn");
     const cancelFormButton = document.getElementById("cancelBtn");
 
     function changeFormState() {
@@ -9,20 +11,55 @@ export default function buildFormElement() {
     }
 
     function changeAddNoteState() {
-        if (addNoteBox.style.display === "static") {
-            addNoteBox.style.display = "none";
+        addNoteBox.style.display === "block" ? "none" : "block";
+    }
+
+    function changeAddNoteFormElement() {
+        addNoteFormElement.style.display === "block" ? "none" : "block";
+    }
+
+    function changeAddNoteButton() {
+        addNoteBox.style.display === "block" ? "none" : "block";
+    }
+
+    function handleAddNoteFormElement(e) {
+        e.preventDefault()
+        // value of Form gets submitted and stored // handle stuff
+    }
+
+    // Add Note Form Element
+    function showAddNoteFormElement(inputBool) {
+        // Eventlistener is added to Note Element
+        if (inputBool) {
+            // Note Form Element gets contrcuted
+            changeAddNoteFormElement();
+            addNoteFormElement.addEventListener("click", handleAddNoteFormElement(e));
+            // Add Note Form Element gets disabled
         } else {
-             addNoteBox.style.display = "static";
+            changeAddNoteFormElement();
+            addNoteFormElement.removeEventListener("click", handleAddNoteFormElement);
         }
     }
+
+    // Add Note
+    function showAddNoteButton(inputBool) {
+        if (inputBool) {
+            showAddNoteFormElement(false);
+            // Add NoteButton is displayed
+            changeAddNoteButton()
+            addNoteBox.addEventListener("click", showAddNoteFormElement(true))
+        } else {
+            changeAddNoteButton();
+            addNoteBox.removeEventListener("click", showAddNoteFormElement);
+        }       
+         
+    }
+   
 
     function createToDo(project = "none") {
         console.log("button pressed");
         changeFormState();
         cancelFormButton.addEventListener("click", changeFormState);
-        changeAddNoteState();
-        addNoteBox.addEventListener("click", changeAddNoteState);
-
     }
 
     return { createToDo }
