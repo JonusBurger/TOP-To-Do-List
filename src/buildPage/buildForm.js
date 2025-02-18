28750ddf2a11a098d3c8235290823957f816cd5e
JonusBurger <j.bergner@live.de>
2025-02-18 09:07:46 +0100

    implement Note behaviour

--- src/buildPage/buildForm.js
+++ src/buildPage/buildForm.js
@@ -10,56 +10,64 @@ export default function buildFormElement() {
         formElement.style.display === "grid" ? formElement.style.display = "none" : formElement.style.display = "grid";
     }
 
-    function changeAddNoteState() {
-        addNoteBox.style.display === "block" ? "none" : "block";
-    }
-
-    function changeAddNoteFormElement() {
-        addNoteFormElement.style.display === "block" ? "none" : "block";
-    }
-
-    function changeAddNoteButton() {
-        addNoteBox.style.display === "block" ? "none" : "block";
-    }
-
-    function handleAddNoteFormElement(e) {
-        e.preventDefault()
-        // value of Form gets submitted and stored // handle stuff
-    }
-
-    // Add Note Form Element
-    function showAddNoteFormElement(inputBool) {
-        // Eventlistener is added to Note Element
-        if (inputBool) {
-            // Note Form Element gets contrcuted
-            changeAddNoteFormElement();
-            addNoteFormElement.addEventListener("click", handleAddNoteFormElement(e));
-            // Add Note Form Element gets disabled
+    function changeAddNoteFormElement(on) {
+        if (on) {
+            addNoteFormElement.style.display = "flex";
         } else {
-            changeAddNoteFormElement();
-            addNoteFormElement.removeEventListener("click", handleAddNoteFormElement);
+            addNoteFormElement.style.display = "none"
+        }   
+    }
+
+    function changeAddNoteButton(on) {
+        if (on) {
+            addNoteBox.style.display = "flex";
+        } else {
+            addNoteBox.style.display = "none"
+        }       
+    }
+
+    function handleNoteAddFormElements(on) {
+        if (on) {
+            changeAddNoteButton(1);
+            changeAddNoteFormElement(0);
+        } else {
+            changeAddNoteButton(0);
+            changeAddNoteFormElement(1);
         }
     }
 
-    // Add Note
-    function showAddNoteButton(inputBool) {
-        if (inputBool) {
-            showAddNoteFormElement(false);
-            // Add NoteButton is displayed
-            changeAddNoteButton()
-            addNoteBox.addEventListener("click", showAddNoteFormElement(true))
-        } else {
-            changeAddNoteButton();
-            addNoteBox.removeEventListener("click", showAddNoteFormElement);
-        }       
-         
+    // add NoteForm
+    function handleAddNoteFormButton(e) {
+        e.preventDefault();
+        handleNoteAddFormElements(1);
+        addNoteButton.removeEventListener("click", handleAddNoteFormButton);
+        addNoteBox.addEventListener("click", handleAddNoteButton);
+    }
+
+    // Add NoteButton
+    function handleAddNoteButton() {
+        handleNoteAddFormElements(0); 
+        addNoteBox.removeEventListener("click", handleAddNoteButton);
+        addNoteButton.addEventListener("click", handleAddNoteFormButton);
+    }
+
+    function cancelButton(e) {
+        e.preventDefault();
+        changeFormState()
+    }
+
+    function handleNoteFormInput() {
+       // Function for validating input of fields! 
     }
    
 
     function createToDo(project = "none") {
         console.log("button pressed");
         changeFormState();
-        cancelFormButton.addEventListener("click", changeFormState);
+        cancelFormButton.addEventListener("click", cancelButton);
+        handleNoteAddFormElements(1);
+        addNoteBox.addEventListener("click", handleAddNoteButton);
+        const storeNotes = []
     }
 
     return { createToDo }
