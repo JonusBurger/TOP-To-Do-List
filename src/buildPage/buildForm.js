06ab770ce054ab564fde3aabbbb915d624720e3a
JonusBurger <j.bergner@live.de>
2025-02-18 16:47:39 +0100

    implement handle toDo form

--- src/buildPage/buildForm.js
+++ src/buildPage/buildForm.js
@@ -1,13 +1,22 @@
-export default function buildFormElement() {
-    const formElement = document.getElementById("formContainer");
+import Note from "../note"
+import ToDo from "../to-do"
+
+export default function buildFormElement(userInfo) {
+    const formContainer = document.getElementById("formContainer");
+    const formElement = document.getElementById("formMain");
+
+    const createButton = document.getElementById("createBtn");
 
     const addNoteFormElement = document.getElementById("noteBox");
     const addNoteBox = document.querySelector(".addNoteBox");
     const addNoteButton = document.getElementById("addNoteBtn");
     const cancelFormButton = document.getElementById("cancelBtn");
 
+    // Info Storage
+    let storeNotes = []
+
     function changeFormState() {
-        formElement.style.display === "grid" ? formElement.style.display = "none" : formElement.style.display = "grid";
+        formContainer.style.display === "grid" ? formContainer.style.display = "none" : formContainer.style.display = "grid";
     }
 
     function changeAddNoteFormElement(on) {
@@ -38,10 +47,15 @@ export default function buildFormElement() {
 
     // add NoteForm
     function handleAddNoteFormButton(e) {
+        let formStatus = addNoteFormElement.checkValidity();
+        addNoteFormElement.reportValidity()
+        if (formStatus) {
+            handleNoteAddFormElements(1);
+            handleNoteFormInput();
+            addNoteButton.removeEventListener("click", handleAddNoteFormButton);
+            addNoteBox.addEventListener("click", handleAddNoteButton);
+        }
         e.preventDefault();
-        handleNoteAddFormElements(1);
-        addNoteButton.removeEventListener("click", handleAddNoteFormButton);
-        addNoteBox.addEventListener("click", handleAddNoteButton);
     }
 
     // Add NoteButton
@@ -51,23 +65,72 @@ export default function buildFormElement() {
         addNoteButton.addEventListener("click", handleAddNoteFormButton);
     }
 
+    function saveButton(e) {
+        // handle submit for ToDo
+        let formMainStatus = formMain.checkValidity();
+        formMain.reportValidity();
+        if(formMainStatus) {
+            const toDoTitle = document.getElementById("toDoTitle");
+            const toDoDueDate = document.getElementById("toDoDueDate");
+            const toDoDescription = document.getElementById("toDoDescription");
+            const toDoProject = document.getElementById("toDoProject");
+            const toDoPrioritiy = document.getElementById("toDoPriority");
+
+            const toDo = new ToDo(toDoTitle.value, toDoDescription.value, toDoPrioritiy.value, toDoDueDate.value);
+            clearMainForm();
+            changeFormState();
+        }
+        e.preventDefault();
+    }
+
     function cancelButton(e) {
         e.preventDefault();
-        changeFormState()
+        // clear all Form-Elements
+        clearNoteForm();
+        changeFormState();
+    }
+
+    function clearMainForm() {
+        const toDoTitle = document.getElementById("toDoTitle");
+        const toDoDueDate = document.getElementById("toDoDueDate");
+        const toDoDescription = document.getElementById("toDoDescription");
+        const toDoProject = document.getElementById("toDoProject");
+        const toDoPrioritiy = document.getElementById("toDoPriority");
+
+        toDoTitle.value = "";
+        toDoDueDate.value = "";
+        toDoDescription.value = "";
+        toDoProject.value = "none";
+        toDoPrioritiy.value = "low";
+
+    }
+
+    function clearNoteForm() {
+        const noteTitle =  document.getElementById("titleNote");
+        const noteDescription = document.getElementById("descriptionNote");
+        noteTitle.value = "";
+        noteDescription.value = "";
     }
 
     function handleNoteFormInput() {
-       // Function for validating input of fields! 
+       // Function for validating input of fields!
+       const noteTitle =  document.getElementById("titleNote");
+       const noteDescription = document.getElementById("descriptionNote");
+       const noteData = new Note(noteTitle.value, noteDescription.value);
+       clearNoteForm();
     }
    
 
     function createToDo(project = "none") {
         console.log("button pressed");
         changeFormState();
+        createButton.addEventListener("click", saveButton);
         cancelFormButton.addEventListener("click", cancelButton);
         handleNoteAddFormElements(1);
         addNoteBox.addEventListener("click", handleAddNoteButton);
-        const storeNotes = []
+
+        // Initialize Data Storage
+        storeNotes = [];
     }
 
     return { createToDo }
